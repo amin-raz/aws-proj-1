4c3f77a5c08a988fe395dca06c6c6c6bba86ecdf
amin-raz <31388683-amin-raz@users.noreply.replit.com>
2025-02-11 05:15:08 +0000

    User checkpoint: Update styling and remove unnecessary SVGs.  Refactored Tailwind CSS import and removed unused base styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,4 +9,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
